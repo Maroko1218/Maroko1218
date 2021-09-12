62880a9ccb958d16b8362d981363554e355626f0
Maroko1218 <markus.svhn@gmail.com>
2021-09-12 22:46:56 +0200

    Some threading practice

---
+++ OperatingSystem/Assignment 1/main.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <pthread.h>
+
+#define NUM_THREADS 5
+
+
+void* routine(void* args) {
+    printf("Hello I am thread %d!\n", *(int*)args);
+    sleep(1);
+    printf("Goodbye! I was thread %d!\n", *(int*)args);
+    free(args);
+}
+
+int main(int argc, char const *argv[]) {
+    pthread_t th[NUM_THREADS];
+    for (size_t i = 0; i < NUM_THREADS; i++) {
+        int* a = malloc(sizeof(int));
+        *a = i;
+        pthread_create(&th[i], NULL, &routine, a);
+    }
+    for (size_t i = 0; i < NUM_THREADS; i++) {
+        pthread_join(th[i], NULL);
+    }
+    
+    return 0;
+}
