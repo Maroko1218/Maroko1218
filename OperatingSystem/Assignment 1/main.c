688902cb0e943d557efa191fb9b98bad340c5140
Maroko1218 <markus.svhn@gmail.com>
2021-09-13 21:59:18 +0200

    Fixed the issue boss :based:

--- OperatingSystem/Assignment 1/main.c
+++ OperatingSystem/Assignment 1/main.c
@@ -1,65 +1,56 @@
 #include "main.h"
-#include <stdbool.h>
-#include <assert.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <pthread.h>
 
-#define BUFFER_SIZE 5
+#define INPUT_BUFFER_SIZE 5
 
 void* producer(void* args) {
-    int id = *(((thread_args*)args)->id) + 1;
-    sem_t *full = ((thread_args*)args)->full, *empty = ((thread_args*)args)->empty;
-    int *buffer = ((thread_args*)args)->buffer;
-    int sleep_duration = ((thread_args*)args)->sleep;
+    sem_t *full = ((thread_args*)args)->full, *empty = ((thread_args*)args)->empty, *critical_sem = ((thread_args*)args)->critical_sem;
+    int id = *(((thread_args*)args)->id), *buffer = ((thread_args*)args)->buffer, sleep_duration = ((thread_args*)args)->sleep, *index = ((thread_args*)args)->index;
     free(((thread_args*)args)->id);
     free((thread_args*)args);
-    printf("Hello I am producer thread %d!\n", id);
-    int *index;
+    printf("Producer thread: %d initialized\n", id);
     while (true) {
         sleep(sleep_duration);
         sem_wait(empty);
-        *buffer = id;
-        buffer++;
+        sem_wait(critical_sem); //This could've been a mutex instead
+        buffer[(*index)++] = id;
+        sem_post(critical_sem);
         sem_post(full);
     }
 }
 
 void* consumer(void* args) {
-    int id = *(((thread_args*)args)->id) + 1;
-    sem_t *full = ((thread_args*)args)->full, *empty = ((thread_args*)args)->empty;
-    int *buffer = ((thread_args*)args)->buffer;
+    sem_t *full = ((thread_args*)args)->full, *empty = ((thread_args*)args)->empty, *critical_sem = ((thread_args*)args)->critical_sem;
+    int id = *(((thread_args*)args)->id), *buffer = ((thread_args*)args)->buffer, *index = ((thread_args*)args)->index, produce;
     free(((thread_args*)args)->id);
     free((thread_args*)args);
-    printf("Hello I am consumer thread %d!\n", id);
-    int *index;
-    int produce;
+    printf("Consumer thread: %d initialized\n", id);
     while (true) {
         sem_wait(full);
-        buffer--;
-        produce = *buffer;
-        printf("Hello I am consumer thread %d! I just ate: %d!\n", id, produce);
+        sem_wait(critical_sem); //This could've been a mutex instead
+        produce = buffer[--(*index)];
+        buffer[*index] = 0;
+        printf("Consumer thread: %d just consumed: %d\n", id, produce);
+        sem_post(critical_sem);
         sem_post(empty);
     }
 }
 
 int main(int argc, char const *argv[]) {
     int producer_count, consumer_count, buffer_size, sleep_duration;
-    char input_buffer[BUFFER_SIZE];
+    char input_buffer[INPUT_BUFFER_SIZE];
     bool isdone = false;
     do {
         printf("How many producer threads: ");
-        fgets(input_buffer, BUFFER_SIZE, stdin);
+        fgets(input_buffer, INPUT_BUFFER_SIZE, stdin);
         producer_count = atoi(input_buffer);
         printf("How many consumer threads: ");
-        fgets(input_buffer, BUFFER_SIZE, stdin);
+        fgets(input_buffer, INPUT_BUFFER_SIZE, stdin);
         consumer_count = atoi(input_buffer);
         printf("How big is the buffer: ");
-        fgets(input_buffer, BUFFER_SIZE, stdin);
+        fgets(input_buffer, INPUT_BUFFER_SIZE, stdin);
         buffer_size = atoi(input_buffer);
         printf("How long do producers sleep: ");
-        fgets(input_buffer, BUFFER_SIZE, stdin);
+        fgets(input_buffer, INPUT_BUFFER_SIZE, stdin);
         sleep_duration = atoi(input_buffer);
         if (producer_count <= 0 || consumer_count <= 0 || buffer_size <= 0 || sleep_duration <= 0) {
             continue;
@@ -67,34 +58,37 @@ int main(int argc, char const *argv[]) {
         isdone = true;
     } while(!isdone);
 
-    sem_t *full = malloc(sizeof(sem_t)), *empty = malloc(sizeof(sem_t));
+    sem_t *full = malloc(sizeof(sem_t)), *empty = malloc(sizeof(sem_t)), *critical_sem = malloc(sizeof(sem_t));
     sem_init(full, 0, 0);
     sem_init(empty, 0, buffer_size);
+    sem_init(critical_sem, 0, 1);
     pthread_t producers[producer_count];
     pthread_t consumers[consumer_count];
-    int* buffer = calloc(buffer_size, sizeof(int));
-
-    
-
+    int *buffer = calloc(buffer_size, sizeof(int)), *index = malloc(sizeof(int));
+    *index = 0;
     for (size_t i = 0; i < producer_count; i++) {
         thread_args* args = malloc(sizeof(thread_args));
         int* a = malloc(sizeof(int));
-        *a = i;
+        *a = i + 1;
         args->full = full;
         args->empty = empty;
-        args->buffer = buffer;
+        args->critical_sem = critical_sem;
         args->id = a;
+        args->buffer = buffer;
         args->sleep = sleep_duration;
+        args->index = index;
         assert(pthread_create(&producers[i], NULL, &producer, args) == 0);
     }
     for (size_t i = 0; i < consumer_count; i++) {
         thread_args* args = malloc(sizeof(thread_args));
         int* a = malloc(sizeof(int));
-        *a = i;
+        *a = i + 1;
         args->full = full;
         args->empty = empty;
-        args->buffer = buffer;
+        args->critical_sem = critical_sem;
         args->id = a;
+        args->buffer = buffer;
+        args->index = index;
         assert(pthread_create(&consumers[i], NULL, &consumer, args) == 0);  
     }
     while(true);
--- OperatingSystem/Assignment 1/main.h
+++ OperatingSystem/Assignment 1/main.h
@@ -2,13 +2,16 @@
 #define MAIN_H
 
 #include <semaphore.h>
-
+#include <stdbool.h>
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <pthread.h>
 
 typedef struct {
-    sem_t *full, *empty;
-    int* buffer;
-    int* id;
-    int sleep;
+    sem_t *full, *empty, *critical_sem;
+    int *buffer, *id, *index, sleep;
 } thread_args;
 
 #endif /* MAIN_H */
