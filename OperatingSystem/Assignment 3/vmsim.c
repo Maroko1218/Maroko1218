39e17c740c4835345f0ab51b172e58c1d7c682c5
Maroko1218 <markus.svhn@gmail.com>
2021-12-27 10:28:44 +0100

    Start code for assignment 3

--- OperatingSystem/Assignment 3/vmsim.c
+++ OperatingSystem/Assignment 3/vmsim.c
@@ -3,20 +3,45 @@
 #include <string.h>
 
 int main(int argc, char const *argv[]) {
-    char *algorithm, *frames, 
-    
-    
-    if (argc != 7 || argc % 2 == 0) {
-        printf("You entered an incorrect amount of flags\n");
-        return 1;
-    }
-    for (size_t i = 0; i < argc; i++) {
-        if (argv[i][0] != '-') {
-            printf("You entered an incorrect flag\n");
-            return 1;
+    int algorithm_flag = 0, frames_flag = 0, file_flag = 0; //Booleans for checking flags
+    int mode = 0; //Mode will be 1 for FIFO, 2 for Optimal, 3 for Lru
+    int frames = 0;
+    FILE *infile = NULL;
+    for (int i = 1; i < argc; i++) {
+        if (argv[i][0] == '-') {
+            if (argv[i][1] == 'a' && !algorithm_flag) {
+                if (strcmp(argv[++i], "fifo") == 0) {
+                    mode = 1;
+                } else if (strcmp(argv[i], "optimal") == 0) {
+                    mode = 2;
+                } else if (strcmp(argv[i], "lru") == 0) {
+                    mode = 3;
+                } else {
+                    printf("Entered an incorrect algorithm\nThe available options are fifo, optimal or lru\n");
+                    return 1;                
+                }
+                algorithm_flag = 1;
+            } else if (argv[i][1] == 'n' && !frames_flag) {
+                if (atoi(argv[++i]) != 0) {
+                    frames = atoi(argv[i]);
+                } else {
+                    printf("Incorrect amount of frames\nPlease enter an integer\n");
+                    return 1;
+                }
+                frames_flag = 1;
+            } else if (argv[i][1] == 'f' && !file_flag) {
+                infile = fopen(argv[++i], "r");
+                if (infile) {
+                    file_flag = 1;
+                } else {
+                    printf("Error opening specified file\nPlease make sure the file exists\n");
+                    return 1;
+                }
+            } else {
+                printf("Entered an incorrect flagn\nThe available options are -a, -n, -f\n");
+                return 1;
+            }
         }
-        if (!strcmp(argv[i], "-a")) {
-            
-        }
     }
+    return 0;
 }
