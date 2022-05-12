4f04f3200384881bc6ced196b6fc37fa6ac637d3
Maroko1218 <markus.svhn@gmail.com>
2022-05-12 16:43:56 +0200

    Finished Prolog
    Co-authored-by: jeppex1 <jeppex1@users.noreply.github.com>

--- ProgrammingLanguages/Lab3/parser.pl
+++ ProgrammingLanguages/Lab3/parser.pl
@@ -20,7 +20,8 @@ match(L, F) :- L = 'real', F is 264.
 match(L, F) :- name(L, [T|S]), char_type(T, alpha), match_id(S), F is 270.
 match(L, F) :- L = ':=', F is 271.
 match(L, F) :- name(L, [T|S]), char_type(T, digit), match_digit(S), F is 272.
-
+match(L, F) :- L = -1, F is 275.
+match(L, F) :- L = L, F is 273.
 
 match_digit([ ]).
 match_digit([H|T]) :- char_type(H, digit), match_digit(T).
@@ -55,8 +56,15 @@ restsent(_, C, [W1 | Ws ]) :- readword(C, W1, C1), restsent(W1, C1, Ws).
 readword(C, W, _)  :- C = -1, W = C.                    /* added EOF handling */
 readword(C, W, C2) :- C = 58, get0(C1), readwordaux(C, W, C1, C2). 
 readword(C, W, C1) :- single_character( C ), name(W, [C]), get0(C1).
+
+readword(C, W, C2) :- 
+    C>47, C<58,
+    get0(C1),
+    restnum(C1, Cs, C2),
+    name(W, [C|Cs]).
+
 readword(C, W, C2) :-
-    in_word(C, NewC ),
+    in_word(C, NewC),
     get0(C1),
     restword(C1, Cs, C2),
     name(W, [NewC|Cs]).
@@ -65,6 +73,13 @@ readword(_, W, C2) :- get0(C1), readword(C1, W, C2).
 
 readwordaux(C, W, C1, C2) :- C1 = 61, name(W, [C, C1]), get0(C2).
 readwordaux(C, W, C1, C2) :- C1 \= 61, name(W, [C]), C1 = C2. 
+
+restnum(C, [C|Cs], C2) :-
+    C>47, C<58,
+    get0(C1),
+    restnum(C1, Cs, C2).
+
+restnum(C, [ ], C).
 
 restword(C, [NewC|Cs], C2) :-
     in_word(C, NewC),
@@ -83,6 +98,7 @@ single_character(41).                  /* ) */
 single_character(42).                  /* + */
 single_character(43).                  /* * */
 single_character(44).                  /* , */
+single_character(45).                  /* - */
 single_character(59).                  /* ; */
 single_character(58).                  /* : */
 single_character(61).                  /* = */
@@ -141,15 +157,16 @@ var_part --> var, var_dec_list.
 header --> program, id, leftpar, input, comma, output, rightpar, scolon.
 prog --> header, var_part, stat_part.
 
-parser(Tokens, Result) :- (prog(Tokens, Result), Result=[], write('Parse Succeed!')); write('Parse Fail!').
+parser(Tokens, Result) :- (prog(Tokens, Result), Result=[], write('Parse OK!')); write('Parse Fail!').
 lab2(File, Result) :- read_in(File, L), lexer(L, Tokens), parser(Tokens, Result).
 
-testall :- working_directory(_, 'testfiles'), testokfiles, testazfiles, testfunfiles, testsemfiles.
+testall :- tell('output.out'), testokfiles, testazfiles, testfunfiles, testsemfiles, told.
 
-testokfiles :- write('Testing OK programs'), nl, nl, parseFiles(['testok1.pas', 'testok2.pas', 'testok3.pas','testok4.pas','testok5.pas','testok6.pas','testok7.pas']).
-testfunfiles :- write('Testing fun programs'), nl, nl, parseFiles(['fun1.pas', 'fun2.pas', 'fun3.pas', 'fun4.pas', 'fun5.pas']).
-testazfiles :- write('Testing a-z programs'), nl, nl, parseFiles(['testa.pas', 'testb.pas', 'testc.pas', 'testd.pas', 'teste.pas', 'testf.pas', 'testg.pas', 'testh.pas', 'testi.pas', 'testj.pas', 'testk.pas', 'testl.pas', 'testm.pas', 'testn.pas', 'testo.pas', 'testp.pas', 'testq.pas', 'testr.pas', 'tests.pas', 'testt.pas', 'testu.pas', 'testv.pas', 'testw.pas', 'testx.pas', 'testy.pas', 'testz.pas']).
-testsemfiles :- write('Testing sem programs'), nl, nl, parseFiles(['sem1.pas', 'sem2.pas', 'sem3.pas', 'sem4.pas', 'sem5.pas']).
+testokfiles :- write('Testing OK programs'), nl, nl, parseFiles(['testfiles/testok1.pas', 'testfiles/testok2.pas', 'testfiles/testok3.pas','testfiles/testok4.pas','testfiles/testok5.pas','testfiles/testok6.pas','testfiles/testok7.pas']).
+testfunfiles :- write('Testing fun programs'), nl, nl, parseFiles(['testfiles/fun1.pas', 'testfiles/fun2.pas', 'testfiles/fun3.pas', 'testfiles/fun4.pas', 'testfiles/fun5.pas']).
+testazfiles :- write('Testing a-z programs'), nl, nl, parseFiles(['testfiles/testa.pas', 'testfiles/testb.pas', 'testfiles/testc.pas', 'testfiles/testd.pas', 'testfiles/teste.pas', 'testfiles/testf.pas', 'testfiles/testg.pas', 'testfiles/testh.pas', 'testfiles/testi.pas', 'testfiles/testj.pas', 'testfiles/testk.pas', 'testfiles/testl.pas', 'testfiles/testm.pas', 'testfiles/testn.pas', 'testfiles/testo.pas', 'testfiles/testp.pas', 'testfiles/testq.pas', 'testfiles/testr.pas', 'testfiles/tests.pas', 'testfiles/testt.pas', 'testfiles/testu.pas', 'testfiles/testv.pas', 'testfiles/testw.pas', 'testfiles/testx.pas', 'testfiles/testy.pas', 'testfiles/testz.pas']).
+testsemfiles :- write('Testing sem programs'), nl, nl, parseFiles(['testfiles/sem1.pas', 'testfiles/sem2.pas', 'testfiles/sem3.pas', 'testfiles/sem4.pas', 'testfiles/sem5.pas']).
 
 parseFiles([ ]).
-parseFiles([H|T]) :- write('Testing '), write(H), nl, nl, read_in(H,L), lexer(L, Tokens), write(Tokens), nl, parser(Tokens, Result), Result=[], nl, write(H), write(' end of parse'), nl, nl, parseFiles(T). 
+parseFiles([H|T]) :- write('Testing '), write(H), nl, nl, read_in(H,L), write(L), nl, lexer(L, Tokens), write(Tokens), nl, parser(Tokens, Result), Result=[], nl, write(H), write(' end of parse'), nl, nl, parseFiles(T).
+
