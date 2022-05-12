f7912f186e91d8914b3e2dd208ad12e0116c3a46
Maroko1218 <markus.svhn@gmail.com>
2022-05-12 21:29:00 +0200

    Python Parser Concept

---
+++ ProgrammingLanguages/Lab4/testfiles/test2.pas
@@ -0,0 +1,6 @@
+program testok1 ( input , output ) ;
+var A , B , C : integer ;  
+
+begin
+3 := B + C * 2  
+end .
---
+++ ProgrammingLanguages/Lab4/testfiles/test3.pas
@@ -0,0 +1,6 @@
+program testok1 ( input , output ) ;
+var A , B , C : real ;  
+
+begin
+3 := B + C * 2
+end .
